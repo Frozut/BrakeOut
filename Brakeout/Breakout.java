c3ef8422886b3d425c369b0c69e064572cc560ea
Delfa <85013277+Frozut@users.noreply.github.com>
2022-03-25 09:00:43 +0100

    Add files via upload

---
+++ Brakeout/Breakout.java
@@ -0,0 +1,25 @@
+package Brakeout;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Breakout extends JFrame {
+    public Breakout(){
+        initUI();
+    }
+
+    private void initUI(){
+        add(new Board());
+        setTitle("minchia che giocho");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        pack();
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(()->{
+            var game=new Breakout();
+            game.setVisible(true);
+        });
+    }
+}
